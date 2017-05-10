b393d3026f8547a435d4d402ac2a80c6a8ecdb35
Stig Dahl <stig@charlottendal.net>
2017-05-10 16:31:03 +0200

    Packages-selected-packages updated in customize.

--- customize.el
+++ customize.el
@@ -7,7 +7,10 @@
    (quote
     ("98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "fee4e306d9070a55dce4d8e9d92d28bd9efe92625d2ba9d4d654fc9cd8113b7f" "dd4628d6c2d1f84ad7908c859797b24cc6239dfe7d71b3363ccdd2b88963f336" "bb4733b81d2c2b5cdec9d89c111ef28a0a8462a167d411ced00a77cfd858def1" "bfdcbf0d33f3376a956707e746d10f3ef2d8d9caa1c214361c9c08f00a1c8409" "6145e62774a589c074a31a05dfa5efdf8789cf869104e905956f0cbd7eda9d0e" "6271fc9740379f8e2722f1510d481c1df1fcc43e48fa6641a5c19e954c21cc8f" default)))
  '(inhibit-startup-screen t)
- '(line-number-mode nil))
+ '(line-number-mode nil)
+ '(package-selected-packages
+   (quote
+    (git-commit zenburn-theme yara-mode wiki-summary which-key use-package switch-window super-save ssh-config-mode search-web powershell powerline paradox mediawiki material-theme markdown-preview-mode magit linum-relative linum-off inkpot-theme gitconfig-mode dos company buffer-move browse-at-remote base16-theme auto-org-md auto-indent-mode ahk-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
