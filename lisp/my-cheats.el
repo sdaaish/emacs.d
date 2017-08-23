0568fbd4c89a3488c3c8dab2aaee165c10564f34
Stig Dahl <stig@charlottendal.net>
2017-08-23 17:09:09 +0200

    Added stuff.

--- lisp/my-cheats.el
+++ lisp/my-cheats.el
@@ -5,24 +5,31 @@
 (cheatsheet-add-group 'Common
                       '(:key "C-x C-c" :description "leave Emacs")
                       '(:key "C-x C-f" :description "find file")
-                      '(:key "C-c s" :description "Show cheatsheet"))
+                      '(:key "C-c s" :description "Show cheatsheet")
+                      '(:key "C-h B" :description "Show my keybindings"))
 
 (cheatsheet-add-group 'IDO
                       '(:key "C-x b" :description "Switch buffers")
                       '(:key "C-f" :description "find file within IDO")
-                      '(:key "C-SPC" :description "Limit listto filter")
+                      '(:key "C-SPC" :description "Limit list to filter")
                       '(:key "C-h f ido-find-file RET" :description "Find ido-documentation"))
 
 (cheatsheet-add-group 'Org-mode
                       '(:key "C-c a t" :description "Start agenda Todo")
-                      '(:key "C-x C-e" :description "export org-mode"))
+                      '(:key "C-c C-e" :description "export org-mode")
+                      '(:key "C-x C-e" :description "eval-last-sexp")
+                      '(:key "<f8> o o" :description "Switch Org buffers"))
 
 (cheatsheet-add-group 'Hydras
                       '(:key "<f8> i" :description "Emacs config files")
-                      '(:key "<f8> " :description "Org-mode stuff")
+                      '(:key "<f8> o" :description "Org-mode stuff")
                       '(:key "C-c b" :description "Launch shells and software")
                       '(:key "C-c v" :description "Hydra toggle functions")
                       '(:key "C-c o" :description "Org-inserts"))
+
 (cheatsheet-add-group 'Company
                       '(:key "M-n" :description "Select next")
                       '(:key "M-p" :description "Select previous"))
+
+(cheatsheet-add-group 'LISP
+                      '(:key "C-x ESC ESC" :description "Repeat last command"))
