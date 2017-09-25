287f215752bdfc27d092b75418f4a8cc141255ed
Stig Dahl <stig@charlottendal.net>
2017-09-25 17:20:05 +0200

    Added org-beautify.

--- customize.el
+++ customize.el
@@ -11,7 +11,7 @@
    [unspecified "#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"] t)
  '(custom-safe-themes
    (quote
-    ("5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "fee4e306d9070a55dce4d8e9d92d28bd9efe92625d2ba9d4d654fc9cd8113b7f" "dd4628d6c2d1f84ad7908c859797b24cc6239dfe7d71b3363ccdd2b88963f336" "bb4733b81d2c2b5cdec9d89c111ef28a0a8462a167d411ced00a77cfd858def1" "bfdcbf0d33f3376a956707e746d10f3ef2d8d9caa1c214361c9c08f00a1c8409" "6145e62774a589c074a31a05dfa5efdf8789cf869104e905956f0cbd7eda9d0e" "6271fc9740379f8e2722f1510d481c1df1fcc43e48fa6641a5c19e954c21cc8f" default)))
+    ("dcf7154867ba67b250fe2c5cdc15a7d170acd9cbe6707cc36d9dd1462282224d" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "fee4e306d9070a55dce4d8e9d92d28bd9efe92625d2ba9d4d654fc9cd8113b7f" "dd4628d6c2d1f84ad7908c859797b24cc6239dfe7d71b3363ccdd2b88963f336" "bb4733b81d2c2b5cdec9d89c111ef28a0a8462a167d411ced00a77cfd858def1" "bfdcbf0d33f3376a956707e746d10f3ef2d8d9caa1c214361c9c08f00a1c8409" "6145e62774a589c074a31a05dfa5efdf8789cf869104e905956f0cbd7eda9d0e" "6271fc9740379f8e2722f1510d481c1df1fcc43e48fa6641a5c19e954c21cc8f" default)))
  '(fci-rule-character-color "#d9d9d9")
  '(fci-rule-color "#37474f")
  '(global-company-mode t)
@@ -21,7 +21,7 @@
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (htmlize ox-publish try afternoon-theme ahk-mode auto-indent-mode auto-org-md base16-theme better-shell bind-key bs buffer-move company copy-as-format dired+ epa-file es-mode gitattributes-mode gitconfig-mode gitignore-mode hydra ido ido-completing-read+ inkpot-theme ispell linum-off linum-relative magit markdown-mode markdown-preview-mode material-theme mediawiki move-text org-bullets org-crypt org-mobile org2jekyll ox-gfm paradox powerline powershell saveplace ssh-config-mode super-save switch-window theme-looper twilight-bright-theme uniquify which-key wiki-summary windresize winner xkcd yara-mode zenburn-theme)))
+    (org-beutify-theme htmlize ox-publish try afternoon-theme ahk-mode auto-indent-mode auto-org-md base16-theme better-shell bind-key bs buffer-move company copy-as-format dired+ epa-file es-mode gitattributes-mode gitconfig-mode gitignore-mode hydra ido ido-completing-read+ inkpot-theme ispell linum-off linum-relative magit markdown-mode markdown-preview-mode material-theme mediawiki move-text org-bullets org-crypt org-mobile org2jekyll ox-gfm paradox powerline powershell saveplace ssh-config-mode super-save switch-window theme-looper twilight-bright-theme uniquify which-key wiki-summary windresize winner xkcd yara-mode zenburn-theme)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(tls-checktrust t)
  '(vc-annotate-background nil)
