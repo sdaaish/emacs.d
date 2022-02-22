15c6dd8e6d89480925fe4373605e25af450393d7
Stig Dahl <stig@charlottendal.net>
2022-02-22 23:07:03 +0100

    Some cleaning up

--- customize.el
+++ customize.el
@@ -49,7 +49,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "CaskaydiaCove Nerd Font" :foundry "SAJA" :slant normal :weight normal :height 98 :width normal))))
  '(Man-overstrike ((t (:inherit bold :foreground "orange"))))
  '(bookmark-face ((t (:background "LightSteelBlue1" :foreground "Black"))))
  '(flycheck-error-list-highlight ((t (:inherit rosy\ brown))))
