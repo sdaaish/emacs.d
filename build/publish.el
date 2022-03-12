cc0eeb69ce2b5c615ebfc16e50abeb787055c3c3
sdaaish <stig@charlottendal.net>
2022-03-12 17:50:37 +0000

    Deploying to html from @ sdaaish/emacs.d@30614b6e139b41eea8600ac59b6e8880ea1d1fb0 🚀

--- build/publish.el
+++ build/publish.el
@@ -1,5 +1,30 @@
 ;;; Publishes html-files from Org
 
+;; Install straight
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Also install use-package
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+;; Install org-mode
+(use-package org
+  :straight org-contrib)
+
+;; Install updated version of htmlize
+(use-package htmlize)
+
 ;; No backups
 (setq make-backup-files nil)
 
