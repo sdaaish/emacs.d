7a86f3254b7fed7351812ed019f717b30414cfda
sdaaish <stig@charlottendal.net>
2022-05-10 18:52:46 +0000

    Deploying to html from @ sdaaish/emacs.d@7c84b590e6fde2ee35087f11c8d0e7d1423d3109 🚀

--- build/publish.el
+++ build/publish.el
@@ -31,6 +31,7 @@
 (require 'ox-publish)
 (setq org-export-html-validation-link nil)
 (setq org-html-inline-images t)
+(setq org-html-head-include-scripts t)
 
 ;; Publish org-files
 (setq org-publish-project-alist
